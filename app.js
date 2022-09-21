cfdb2b3ba8b604855ec3145f251786ce1fb2d116
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-21 13:24:17 +0530

    group asscociations

--- app.js
+++ app.js
@@ -30,9 +30,24 @@ const sequelize = require("./Backend/server/util/database");
 
 const User = require("./Backend/server/src/models/signup");
 const Message = require("./Backend/server/src/models/message");
+const Group = require("./Backend/server/src/models/group");
+const GroupMembers = require("./Backend/server/src/models/groupMembers");
+const GroupMessages = require("./Backend/server/src/models/groupMessage");
 
 User.hasMany(Message);
 Message.belongsTo(User);
+
+User.belongsToMany(Group, { through: GroupMembers });
+Group.belongsToMany(User, { through: GroupMembers });
+
+Group.hasMany(User);
+User.belongsTo(Group);
+
+Group.hasMany(GroupMessages);
+GroupMessages.belongsTo(Group);
+
+User.hasMany(GroupMessages);
+GroupMessages.belongsTo(User);
 
 sequelize
   .sync()
