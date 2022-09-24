23a91e3a26e52ff52775fdc2bb2a6ea12ae30326
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-25 02:24:48 +0530

    chat with anyone feature added

--- Backend/server/controllers/chat.js
+++ Backend/server/controllers/chat.js
@@ -20,40 +20,56 @@ exports.getUsers = async (req, res, next) => {
 };
 
 exports.getChat = async (req, res, next) => {
-  const lastMsg = +req.query.lastMsg;
+  const receiverId = req.body.rec_id;
+  console.log(req.id, receiverId);
+  const messages = await Message.findAll({
+    where: {
+      [Op.or]: [
+        { userId: req.id, receiver: receiverId },
+        { userId: receiverId, receiver: req.id },
+      ],
+    },
+  });
 
-  await Message.findAll({ where: { id: { [Op.gt]: lastMsg } } })
-    .then((messages) => {
-      res.status(200).json({
-        success: true,
-        chat: messages,
-        message: "chat send successfully",
-      });
-    })
-    .catch((err) => {
-      res
-        .status(403)
-        .json({ success: true, error: err, message: "something went wrong" });
-    });
+  res.status(200).json({
+    success: true,
+    chat: messages,
+    message: "chat send successfully",
+  });
+
+  //const lastMsg = +req.query.lastMsg;
+  //await Message.findAll({ where: { id: { [Op.gt]: lastMsg } } })
+  //  .then((messages) => {
+  //    res.status(200).json({
+  //      success: true,
+  //      chat: messages,
+  //      message: "chat send successfully",
+  //    });
+  //  })
+  //  .catch((err) => {
+  //    res
+  //      .status(403)
+  //      .json({ success: true, error: err, message: "something went wrong" });
+  //  });
 };
 
 exports.postChat = async (req, res, next) => {
   const id = req.id;
   const msg = req.body.message;
+  const receiverId = req.body.receiverId;
 
-  const user = await User.findOne({ where: { id: id } })
-    .then((user) => {
-      return user.name;
-    })
-    .catch(() => {
-      console.log("in post chat error");
-    });
+  const user = await User.findByPk(id).then((user) => {
+    return user.name;
+  });
 
-  await Message.create({ username: user, message: msg, userId: id }).then(
-    (result) => {
-      return res
-        .status(201)
-        .json({ result: result, success: true, message: "added message" });
-    }
-  );
+  await Message.create({
+    username: user,
+    message: msg,
+    userId: id,
+    receiver: receiverId,
+  }).then((result) => {
+    return res
+      .status(201)
+      .json({ result: result, success: true, message: "added message" });
+  });
 };
